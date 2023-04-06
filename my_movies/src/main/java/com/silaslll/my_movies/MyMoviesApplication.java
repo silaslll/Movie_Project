fa1d193a07fee8a596f4241b2747d863aaa5e469
silaslll <lihangyi01@gmail.com>
2023-04-06 16:24:52 -0400

    connect to Mangodb

--- my_movies/src/main/java/com/silaslll/my_movies/MyMoviesApplication.java
+++ my_movies/src/main/java/com/silaslll/my_movies/MyMoviesApplication.java
@@ -2,12 +2,20 @@ package com.silaslll.my_movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MyMoviesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyMoviesApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String firstEndpoint(){
+		return "Hello World";
 	}
 
 }
