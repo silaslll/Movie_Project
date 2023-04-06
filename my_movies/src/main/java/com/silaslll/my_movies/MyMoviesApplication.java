2253efa5d490b4d15d8c88f5a5747c9f01cee580
silaslll <lihangyi01@gmail.com>
2023-04-06 14:36:04 -0400

    add spring backend

---
+++ my_movies/src/main/java/com/silaslll/my_movies/MyMoviesApplication.java
@@ -0,0 +1,13 @@
+package com.silaslll.my_movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMoviesApplication.class, args);
+	}
+
+}
---
+++ my_movies/src/test/java/com/silaslll/my_movies/MyMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silaslll.my_movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
