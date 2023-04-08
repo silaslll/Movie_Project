db235e755586aaef45ca2362da512320fca195a3
silaslll <lihangyi01@gmail.com>
2023-04-07 22:48:14 -0400

    add Trailer

--- Frontend/movie_client/src/App.js
+++ Frontend/movie_client/src/App.js
@@ -5,7 +5,7 @@ import Layout from './component/Layout';
 import {Routes, Route} from 'react-router-dom';
 import Home from './component/home/Home';
 import Header from './component/header/Header';
-
+import Trailer from './component/trailer/Trailer';
 
 function App() {
   const [movies, setMovies] = useState([]);
@@ -37,6 +37,7 @@ useEffect(() => {
     <Routes>
           <Route path="/" element={<Layout/>}>
           <Route path="/" element={<Home movies={movies} />} ></Route>
+          <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
 
           </Route>
       </Routes>
